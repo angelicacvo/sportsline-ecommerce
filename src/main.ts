3b0081e109c80f257f31fbff618b5f3e32429ecd
Angelica Cuervo <108093638+angelicacvo@users.noreply.github.com>
2025-11-06 07:41:42 -0500

    feat: using typeORM to connect supabase database succesfully

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,16 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ConfigService } from '@nestjs/config';
+import { config } from 'process';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(process.env.PORT ?? 3000);
+  const config = app.get(ConfigService);
+
+  app.enableCors();
+
+  const port = config.get<number>('PORT') || 3000;
+  await app.listen(port);
+  console.log(`Application is running on: http://localhost:${port}`);
 }
 bootstrap();
